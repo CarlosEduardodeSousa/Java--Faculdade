d3e3e41aecd5499885795fb96bd2ec09cdd16bf5
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2024-08-24 14:37:26 -0300

    Create Helloworld2.java

---
+++ Helloworld2.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Helloworld2 {
+    public static void main(String[] args){
+        System.out.println("Olá, Mundo");
+        JOptionPane.showMessageDialog(null, "Olá, Mundo");
+    }
+}
